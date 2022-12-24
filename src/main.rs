dbf9d1c9c444117ba3e38a8d098fdea421a1a3b8
nesmy <pnesmy@gmail.com>
2022-12-24 16:14:04 -0500

    vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
